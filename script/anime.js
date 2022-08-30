6386aa241f7dcae5a8c6db59050b1511a330f774
p34-pac <92242499+p34-pac@users.noreply.github.com>
2022-08-30 15:44:34 +0100

    my portfolio

---
+++ script/anime.js
@@ -0,0 +1,30 @@
+const menus = document.querySelector('.menu');
+const dsh = document.querySelector('.dsh');
+const cancel = document.querySelector('.cancel-box');
+cancel.addEventListener('click', clearMenu)
+
+dsh.addEventListener('click', showMenu);
+// menus.addEventListener('click', stay);
+// function stay(){
+//     menus.style.display = 'block';
+
+// }
+function showMenu(e){
+    menus.style.display = 'block';
+    // if(menus.style.right == '-100px'){
+    //     alert('yes')
+    // }
+
+    dsh.style.display='none';
+}
+// function none(){
+//     menus.style.display = 'none';
+//     dsh.style.display='block';
+// }
+function clearMenu(){
+    menus.style.display = 'none';
+    dsh.style.display='block';
+        
+
+    
+}
